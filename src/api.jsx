800f4214d36f93aee85fa23f9042ba55eec93034
FabricioDangellis <fabriciodangellisfdf@gmail.com>
2024-10-16 08:50:33 -0300

    t

--- src/api.jsx
+++ src/api.jsx
@@ -3,7 +3,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:8081',  // Atualize para o endpoint do seu backend
+    baseURL: 'http://localhost:3000',  // Atualize para o endpoint do seu backend
 });
 
 export default api;
