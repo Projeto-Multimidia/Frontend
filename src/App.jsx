26ffdb7de2df84c652590c4ceb24fed4c9c8f453
FabricioDangellis <fabriciodangellisfdf@gmail.com>
2024-10-16 21:34:11 -0300

    ADD: Tela de Home e Perfil do usuário

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import Home from './pages/HomePage';
+import Profile from './pages/Profile';
 
 function App() {
     return (
@@ -8,6 +10,8 @@ function App() {
             <Routes>
                 <Route path="/" element={<Login />} />
                 <Route path="/cadastro" element={<Register />} />
+                <Route path="/home" element={<Home />} />
+                <Route path="/profile" element={<Profile />} />
             </Routes>
         </Router>
     );
